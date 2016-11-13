ea2a9b3d2c2077d03e02cdcb998f6e0e6f968f4b
Kris <kris@meridian-ds.com>
2016-11-13 13:40:15 -0600

    updating DefaultController to use the new service, removing commented lines from .install

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,30 +1,50 @@
-<?php /**
- * @file
- * Contains \Drupal\pants\Controller\DefaultController.
- */
+<?php
 
 namespace Drupal\pants\Controller;
 
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\pants\PantsColor;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Default controller for the pants module.
  */
 class DefaultController extends ControllerBase {
 
+  /**
+   * The pants color service.
+   *
+   * @var \Drupal\pants\PantsColor
+   */
+  protected $pantsColor;
+
+  /**
+   * DefaultController constructor.
+   *
+   * @param \Drupal\pants\PantsColor $pants_color
+   *   The pants color service.
+   */
+  public function __construct(PantsColor $pants_color) {
+    $this->pantsColor = $pants_color;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static($container->get('pants.color'));
+  }
 
   public function colorAccess() {
     return AccessResult::allowedIf(\Drupal::currentUser()->hasPermission('access pants color'));
   }
 
   public function color(AccountInterface $user) {
-    $config = \Drupal::config('pants.settings');
-    $pants_color = isset($user->pants_color->value) ? $user->pants_color->value : $config->get('default_color');
-
     return [
-      '#markup' => $pants_color,
+      '#markup' => $this->pantsColor->getPantsColor($user),
     ];
   }
+
 }
